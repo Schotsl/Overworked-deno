23ae01a53ab3dd53c77e81c89ae71c57b35c6df3
Sjors van Holst <sjorsvanholst@gmail.com>
2022-02-24 17:41:19 +0100

    🎨 Formatted codebase

--- entity/PersonEntity.ts
+++ entity/PersonEntity.ts
@@ -6,10 +6,9 @@ import {
 import BaseEntity from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/entity/BaseEntity.ts";
 
 export default class PersonEntity extends BaseEntity {
+  public image = new UUIDColumn("image", false);
   public first = new VarcharColumn("first");
   public last = new VarcharColumn("last");
-
-  public image = new UUIDColumn("image", false);
 
   constructor() {
     super();
