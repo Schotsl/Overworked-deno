0c047249b7f4ce7cb7831164031169fa753ac021
Sjors van Holst <sjorsvanholst@gmail.com>
2022-02-24 17:41:38 +0100

    🐛 Fixed name bug

--- collection/LocationCollection.ts
+++ collection/LocationCollection.ts
@@ -1,6 +1,6 @@
 import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/collection/BaseCollection.ts";
 import LocationEntity from "../entity/LocationEntity.ts";
 
-export default class HistoryCollection extends BaseCollection {
+export default class LocationCollection extends BaseCollection {
   public locations: LocationEntity[] = [];
 }
