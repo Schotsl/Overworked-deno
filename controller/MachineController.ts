cfe967f048c0b785f17e69c562da6034448e120c
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-26 17:17:12 +0200

    ✨ Added machine endpoint

---
+++ controller/MachineController.ts
@@ -0,0 +1,79 @@
+import { renderREST } from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/helper.ts";
+import {
+  Request,
+  Response,
+  State,
+} from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+import MachineEntity from "../entity/MachineEntity.ts";
+import MachineCollection from "../collection/MachineCollection.ts";
+import MachineRepository from "../repository/MachineRepository.ts";
+import GeneralController from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/controller/GeneralController.ts";
+import InterfaceController from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/controller/InterfaceController.ts";
+
+export default class MachineController implements InterfaceController {
+  private generalController: GeneralController;
+  private machineRepository: MachineRepository;
+
+  constructor(
+    name: string,
+  ) {
+    this.machineRepository = new MachineRepository(name);
+    this.generalController = new GeneralController(
+      name,
+      MachineEntity,
+      MachineCollection,
+    );
+  }
+
+  async getCollection(
+    { request, response, state }: {
+      request: Request;
+      response: Response;
+      state: State;
+    },
+  ) {
+    const { offset, limit } = state;
+
+    const params = request.url.searchParams;
+    const persons = params.get(`persons`)?.split(",");
+
+    // TODO: Implement validation of persons array
+
+    // TODO: Check if all persons are friends
+
+    const result = await this.machineRepository.getCollection(
+      offset,
+      limit,
+      persons!,
+    );
+
+    const parsed = renderREST(result);
+
+    response.body = parsed;
+  }
+
+  getObject(
+    { response, params }: {
+      response: Response;
+      params: { uuid: string };
+    },
+  ) {
+    return this.generalController.getObject({ response, params });
+  }
+
+  removeObject(
+    { response, params }: {
+      response: Response;
+      params: { uuid: string };
+    },
+  ) {
+    return this.generalController.removeObject({ response, params });
+  }
+
+  addObject(
+    { request, response }: { request: Request; response: Response },
+  ) {
+    return this.generalController.addObject({ request, response });
+  }
+}
