c768c91a03a2785439563ca43cb59ed9924d114c
Wouter de Bruijn <wouter@hedium.nl>
2022-05-26 12:24:34 +0200

    :sparkles: Added Google auth Middleware

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import * as jose from "https://deno.land/x/jose@v4.8.1/index.ts";
+import { Middleware } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+import {
+  MissingAuthentication,
+  InvalidAuthentication,
+} from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/errors.ts";
+
+export const authenticationMiddleware: Middleware = async (
+  { request, state },
+  next
+) => {
+  const token = request.headers.get("Authorization")?.split(" ")[1];
+
+  if (typeof token !== "string") {
+    throw new MissingAuthentication();
+  }
+
+  try {
+    const JWKS = jose.createRemoteJWKSet(
+      new URL("https://www.googleapis.com/oauth2/v3/certs")
+    );
+
+    const { payload } = await jose.jwtVerify(token, JWKS, {
+      issuer: "accounts.google.com",
+    });
+
+    state.user = payload;
+
+    await next();
+  } catch (error) {
+    console.error(error);
+    throw new InvalidAuthentication();
+  }
+};
