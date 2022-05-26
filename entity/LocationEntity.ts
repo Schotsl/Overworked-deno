4d07ffbf95ead66569d3486162ed1da5b25b0873
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-26 23:36:47 +0200

    ✨ Added lat / lng property

--- entity/LocationEntity.ts
+++ entity/LocationEntity.ts
@@ -1,13 +1,15 @@
 import {
-  UUIDColumn,
+  LatColumn,
+  LngColumn,
   VarcharColumn,
 } from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/other/Columns.ts";
 
 import BaseEntity from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/entity/BaseEntity.ts";
 
 export default class LocationEntity extends BaseEntity {
-  public image = new UUIDColumn("image", false);
   public title = new VarcharColumn("title");
+  public lat = new LatColumn("lat");
+  public lng = new LngColumn("lng");
 
   constructor() {
     super();
