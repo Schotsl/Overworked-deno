6f76271526b095e3e1297c82608fc8f4e5c450ae
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-27 12:58:18 +0200

    :recycle:

--- entity/PersonEntity.ts
+++ entity/PersonEntity.ts
@@ -1,15 +1,15 @@
 import {
+  UrlColumn,
   VarcharColumn,
 } from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/other/Columns.ts";
 
 import BaseEntity from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/entity/BaseEntity.ts";
 
 export default class PersonEntity extends BaseEntity {
-  // TODO: Add URL column
-
   public name = new VarcharColumn("name");
-  public photo = new VarcharColumn("photo");
   public email = new VarcharColumn("email");
+  
+  public photo = new UrlColumn("photo");
 
   constructor() {
     super();
