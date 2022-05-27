a2fbc2797d0777c6a2ee96089b554312eccfe799
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-27 12:58:30 +0200

    :art: Formatted codebase

--- entity/PersonEntity.ts
+++ entity/PersonEntity.ts
@@ -8,7 +8,7 @@ import BaseEntity from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/
 export default class PersonEntity extends BaseEntity {
   public name = new VarcharColumn("name");
   public email = new VarcharColumn("email");
-  
+
   public photo = new UrlColumn("photo");
 
   constructor() {
