d4a2812108aa59953888efd3a660f7a826a566df
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-28 23:38:22 +0200

    :recycle: Fixed class names

--- collection/MachineCollection.ts
+++ collection/MachineCollection.ts
@@ -1,6 +1,6 @@
 import MachineEntity from "../entity/MachineEntity.ts";
 import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/collection/BaseCollection.ts";
 
-export default class HistoryCollection extends BaseCollection {
+export default class MachineCollection extends BaseCollection {
   public machines: MachineEntity[] = [];
 }
--- collection/PersonCollection.ts
+++ collection/PersonCollection.ts
@@ -1,6 +1,6 @@
 import PersonEntity from "../entity/PersonEntity.ts";
 import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/collection/BaseCollection.ts";
 
-export default class HistoryCollection extends BaseCollection {
+export default class PersonCollection extends BaseCollection {
   public persons: PersonEntity[] = [];
 }
